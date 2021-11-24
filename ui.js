cc0a9dd6ff2f73ed0d538853c9845c90a09dd61f
Andrii Ostapenko <droid85@yahoo.com>
2021-11-24 20:49:16 +0200

    Update ui.js
    Done with popUpOn and popUpOff functions

--- ui.js
+++ ui.js
@@ -1,8 +1,15 @@
 class RENDEREL {
     containerEl = document.querySelector('#users');
+    createBtnEl = document.querySelector('#create-btn');
+    popUpEl = document.querySelector('#pop-up-bg');
+    formEl = document.querySelector('#form')
+    userInputEl = document.querySelector('.form-input');
+    okBtnEl = document.querySelector('.agree-btn');
     blockEl = 'div';
+    paragraphEl = 'p';
     btnEl = 'button';
     blockClass = 'user-block';
+    paragraphClass = 'user-paragraph';
     btnClass = 'user-btn';
     btnDeleteClass = 'delete-btn';
     btnEditClass = 'edit-btn';
@@ -11,9 +18,26 @@ class RENDEREL {
 
     showUsers(name, id) {
         this.containerEl.insertAdjacentHTML('beforeend', `
-            <${this.blockEl} class = "${this.blockClass}" id = "${id}">${name}
+            <${this.blockEl} class = "${this.blockClass}" id = "${id}">
+            <${this.paragraphEl} class = "${this.paragraphClass}">${name}</${this.paragraphEl}>
             <${this.btnEl} class = "${this.btnClass} ${this.btnEditClass}">${this.editBtnTxt}</${this.btnEl}>
             <${this.btnEl} class = "${this.btnClass} ${this.btnDeleteClass}">${this.deleteBtnText}</${this.btnEl}>
             </${this.blockEl}>`)
     }
+
+    popUpOn() {
+        this.popUpEl.classList.add('activePopUp');
+        this.createBtnEl.classList.add('active-add-btn');
+    }
+
+    popUpOff() {
+        this.userInputEl.value = '';
+        this.createBtnEl.classList.remove('active-add-btn');
+        this.popUpEl.classList.remove('activePopUp');
+    }
+
+    clearForm() {
+        this.formEl.innerHTML = '';
+    }
+
 }
