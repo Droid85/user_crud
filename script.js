326bd7396598033ce9ad361b9a8027189e6750d5
Andrii Ostapenko <droid85@yahoo.com>
2021-11-24 20:46:15 +0200

    Update script.js
    finished init function

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ init();
 function init() {
     const http = new HTTP();
     const ui = new RENDEREL();
+    let userData = {}
 
-    ui.containerEl.addEventListener('click', onClickShowBtn)
+    ui.containerEl.addEventListener('click', onClickUserBtn);
+    ui.createBtnEl.addEventListener('click', onClickCreateBtn);
+    ui.popUpEl.addEventListener('click', onClickConfirmBtn)
 
     http.get(ENVIRONMENT.USERS.getUsers)
     .then((response) => response.json())
@@ -16,16 +19,56 @@ function init() {
     })
     .catch(error => console.log('USERS ERROR', error))
 
-    function onClickShowBtn(e) {
+    function onClickUserBtn(e) {
         let userId = e.target.parentNode.id;
         if (e.target.classList.contains('delete-btn')) {
             http.delete(ENVIRONMENT.USERS.getUsers, userId)
-            .then(response => console.log(response))
+            .then(response => {
+                e.target.parentNode.remove()
+                console.log(response)
+            })
             .catch(error => console.log('DELETE ERROR', error))
         } else if (e.target.classList.contains('edit-btn')) {
-            http.edit(ENVIRONMENT.USERS.getUsers, userId)
+            ui.popUpOn();
+            ui.okBtnEl.id = 'edit-btn-ok';
+            http.get(ENVIRONMENT.USERS.getUsers + userId)
+            .then((response) => response.json())
+            .then(res => {
+                userData = res
+                ui.userInputEl.value = userData.name
+            })
+            .catch(error => console.log('EDIT ERROR', error))
+        }
+    }
+
+    function onClickCreateBtn(e) {
+        if (e.target.classList.contains('add-btn')) {
+            ui.popUpOn()
+            ui.okBtnEl.id = 'add-btn-ok';
+        }
+    }
+
+    function onClickConfirmBtn(e) {
+        if (e.target.id === 'add-btn-ok') {
+            let newUser = {
+                id: Math.random()
+            };
+            newUser.name = ui.userInputEl.value;
+            http.create(ENVIRONMENT.USERS.getUsers, newUser)
+            .then(res => console.log(res))
+            .catch(error => console.log('CREATE ERROR', error))
+            ui.showUsers(newUser.name)
+            ui.popUpOff()
+        } else if (e.target.classList.contains('discard-btn')) {
+            ui.popUpOff()
+        } else if (e.target.id === 'edit-btn-ok') {
+            userData.name = ui.userInputEl.value
+            let editedEl = document.getElementById(userData.id)
+            editedEl.firstElementChild.textContent = ui.userInputEl.value
+            http.edit(ENVIRONMENT.USERS.getUsers, userData.id, userData)
             .then(response => console.log(response))
-            .catch(error => console.log('DELETE ERROR', error))
+            .catch(error => console.log('EDIT ERROR', error))
+            ui.popUpOff()
         }
     }
 }
